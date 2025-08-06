e3b312adc343d90fece67d89585b7befa3764790
LukeQD <157420957+LukeQD@users.noreply.github.com>
2025-08-06 13:49:58 -0400

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Vue Stress-Test</h1>
+  <GraphGrid />
+</template>
+
+<script setup>
+import GraphGrid from './components/GraphGrid.vue'
+</script>
+
+<style>
+h1 { font-family: sans-serif; margin-bottom: 1rem; }
+</style>
