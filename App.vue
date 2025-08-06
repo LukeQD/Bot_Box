ba558bba2059dd9a816b0ca8ca2d3a939e8c2424
LukeQD <157420957+LukeQD@users.noreply.github.com>
2025-08-06 14:05:01 -0400

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,12 +1,36 @@
 <template>
-  <h1>Vue Stress-Test</h1>
-  <GraphGrid />
+  <div>
+    <button @click="addGraph">Add Graph</button>
+    <div class="grid">
+      <iframe
+        v-for="(frame, index) in iframes"
+        :key="index"
+        src="/graph.html"
+        class="iframe-tile"
+      ></iframe>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import GraphGrid from './components/GraphGrid.vue'
+import { ref } from 'vue'
+
+const iframes = ref([])
+
+function addGraph() {
+  iframes.value.push({})
+}
 </script>
 
-<style>
-h1 { font-family: sans-serif; margin-bottom: 1rem; }
+<style scoped>
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.iframe-tile {
+  width: 640px;
+  height: 360px;
+  border: none;
+}
 </style>
